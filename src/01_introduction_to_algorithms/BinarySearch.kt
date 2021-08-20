cf60720ac571ff767ba0cc0773447c5a691f34ed
Falchio <soulsard@gmail.com>
2021-08-20 08:35:54 +0300

    Binary search

---
+++ src/01_introduction_to_algorithms/BinarySearch.kt
@@ -0,0 +1,36 @@
+package `01_introduction_to_algorithms`
+
+fun main() {
+    print(sortedArray.binarySearch(99))
+}
+
+
+val sortedArray = (0..128 step 1).toList()
+
+
+fun List<Int>.binarySearch(item: Int): Int? {
+    var min = 0
+    var max = lastIndex
+    var i = 1
+    while (min <= max) {
+        val mid = (min + max) / 2
+
+        println("min: $min; mid: $mid; max: $max;")
+
+        val guess = this[mid]
+        if (guess == item) {
+            println("число попыток: $i")
+            return mid
+        }
+        if (guess > item) {
+            max = mid - 1
+            println("загадано число: $item; догадка равна ${this[mid]}; снижаем верхнюю границу до: $max")
+        } else {
+            min = mid + 1
+            println("загадано число: $item; догадка равна ${this[mid]}; повышаем нижнюю границу до: $min")
+        }
+        i++
+    }
+    return null
+}
+
